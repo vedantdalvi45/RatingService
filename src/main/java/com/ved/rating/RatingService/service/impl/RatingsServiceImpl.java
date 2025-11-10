79cf44c59f731e693f5e2cb792a82ef428274a90
vedantdalvi45 <vedalvi16@gmail.com>
2025-11-10 20:10:11 +0530

    Added : RatingsServiceImpl all 4 Methods

--- src/main/java/com/ved/rating/RatingService/service/impl/RatingsServiceImpl.java
+++ src/main/java/com/ved/rating/RatingService/service/impl/RatingsServiceImpl.java
@@ -1,30 +1,36 @@
 package com.ved.rating.RatingService.service.impl;
 
 import com.ved.rating.RatingService.model.Rating;
+import com.ved.rating.RatingService.repository.RatingsRepository;
 import com.ved.rating.RatingService.service.RatingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class RatingsServiceImpl implements RatingService {
+
+    @Autowired
+    private RatingsRepository ratingsRepository;
+
     @Override
     public Rating create(Rating rating) {
-        return null;
+        return ratingsRepository.save(rating);
     }
 
     @Override
     public List<Rating> getAllRatings() {
-        return List.of();
+        return ratingsRepository.findAll();
     }
 
     @Override
     public List<Rating> getAllRatingsByUserId(String userId) {
-        return List.of();
+        return ratingsRepository.findByUserId(userId);
     }
 
     @Override
     public List<Rating> getAllRatingsByHotelId(String hotelId) {
-        return List.of();
+        return ratingsRepository.findByHotelId(hotelId);
     }
 }
