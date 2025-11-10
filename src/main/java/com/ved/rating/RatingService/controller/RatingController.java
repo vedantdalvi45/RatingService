9ae89ed7b962a85d63ba49c9c545875d14e71990
vedantdalvi45 <vedalvi16@gmail.com>
2025-11-10 20:20:54 +0530

    Added : RatingController

---
+++ src/main/java/com/ved/rating/RatingService/controller/RatingController.java
@@ -0,0 +1,39 @@
+package com.ved.rating.RatingService.controller;
+
+
+import com.ved.rating.RatingService.model.Rating;
+import com.ved.rating.RatingService.service.RatingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/ratings")
+public class RatingController {
+    @Autowired
+    private RatingService ratingService;
+
+    @PostMapping
+    public ResponseEntity<Rating> createRating(@RequestBody Rating rating) {
+        Rating createdRating = ratingService.create(rating);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdRating);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Rating>> getRatings() {
+        return ResponseEntity.ok(ratingService.getAllRatings());
+    }
+
+    @GetMapping("/hotel/{hotelId}")
+    public ResponseEntity<List<Rating>> getRatingsByHotelId(@PathVariable String hotelId) {
+        return ResponseEntity.ok(ratingService.getAllRatingsByHotelId(hotelId));
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Rating>> getRatingsByUserId(@PathVariable String userId) {
+        return ResponseEntity.ok(ratingService.getAllRatingsByUserId(userId));
+    }
+}
