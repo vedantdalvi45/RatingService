815d4de884f61d4eaa040896606b5a7b80335d7e
vedantdalvi45 <vedalvi16@gmail.com>
2025-11-10 20:09:32 +0530

    Added : findByUserId & findByHotelId into RatingsRepository

--- src/main/java/com/ved/rating/RatingService/repository/RatingsRepository.java
+++ src/main/java/com/ved/rating/RatingService/repository/RatingsRepository.java
@@ -3,5 +3,10 @@ package com.ved.rating.RatingService.repository;
 import com.ved.rating.RatingService.model.Rating;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface RatingsRepository extends MongoRepository<Rating, String> {
+    List<Rating> findByUserId(String userId);
+
+    List<Rating> findByHotelId(String hotelId);
 }
